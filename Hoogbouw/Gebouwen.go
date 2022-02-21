89a3a9d95be5299df9a7c0ce5295cf1ce1e0534d
Tijs Manders <tijs.manders@student.fontys.nl>
2022-02-21 10:34:57 +0100

    input gebruiker v1 verwerkt

--- Hoogbouw/Gebouwen.go
+++ Hoogbouw/Gebouwen.go
@@ -1,9 +1,41 @@
 package Hoogbouw
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func Hoogteverschil() {
-	fmt.Print()
+	// Geef 3 gebouwen met hoogtes op
+	fmt.Println("Wat is de naam van het eerste gebouw?")
+	// bufio.newscanner is maar 1 keer nodig?
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	NaamGebouw1 := input.Text()
+	fmt.Println("Wat is de hoogte van", NaamGebouw1, "in meters?")
+	input.Scan()
+	HoogteGebouw1 := input.Text()
+
+	fmt.Println("Wat is de naam van het tweede gebouw?")
+	input.Scan()
+	NaamGebouw2 := input.Text()
+	fmt.Println("Wat is de hoogte van", NaamGebouw2, "in meters?")
+	input.Scan()
+	HoogteGebouw2 := input.Text()
+
+	fmt.Println("Wat is de naam van het derde gebouw?")
+	input.Scan()
+	NaamGebouw3 := input.Text()
+	fmt.Println("Wat is de hoogte van", NaamGebouw3, "in meters?")
+	input.Scan()
+	HoogteGebouw3 := input.Text()
+
+	fmt.Println("De namen van de eerste 3 gebouwen zijn:", NaamGebouw1, NaamGebouw2, NaamGebouw3, "En de hoogte hiervan zijn:", HoogteGebouw1, HoogteGebouw2, HoogteGebouw3)
+
+	//fmt.Println("Wil je nog meer gebouwen toevoegen?")
+
+	// Rekensom
+
+	// Uitslag wegschrijven naar tekstbestand
 }
